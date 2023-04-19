284cdcf111ae5718d0c6d79a4e38cd9f057367de
Shubham Tiwari <st7112001@gmail.com>
2023-04-19 23:31:08 +0530

    MongoDB connected

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 // 1)-> Initialization
 const express = require('express');
-
 const app = express();
 
-// 2)-> App Routing
+const mongoose = require('mongoose');
 
-// Home Page Route
-app.get("/", function(req,res){
-    res.send("This is the home page");
+mongoose.connect("mongodb+srv://ShubhamHarsh:Pb3zgpYV61xZv4lT@cluster0.jxblg4i.mongodb.net/?retryWrites=true&w=majority").then(function(){
+    app.get("/", function(req,res){
+        res.send("This is the home page");
+    });
+
+    app.get("/notes", function(req,res){
+        res.send("This is the Notes page");
+    });
 });
 
-// Notes Route
-app.get("/notes", function(req,res){
-    res.send("This is the Notes page");
-});
+
 
 // 3)-> Starting the server on a port
 
-app.listen(5000); // port
+app.listen(5000, function(){
+    console.log("Server started at PORT : 5000");
+}); // port
