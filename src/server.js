16e304cb9ed81eea184290f2516b4a56c9e9f580
Shubham Tiwari <st7112001@gmail.com>
2023-04-20 12:32:51 +0530

    get completed

--- src/server.js
+++ src/server.js
@@ -3,14 +3,34 @@ const express = require('express');
 const app = express();
 
 const mongoose = require('mongoose');
+const Note = require('./models/Note');
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false})); // Nested objects for true
+app.use(bodyParser.json());
 
 mongoose.connect("mongodb+srv://ShubhamHarsh:Pb3zgpYV61xZv4lT@cluster0.jxblg4i.mongodb.net/?retryWrites=true&w=majority").then(function(){
     app.get("/", function(req,res){
         res.send("This is the home page");
     });
 
-    app.get("/notes", function(req,res){
-        res.send("This is the Notes page");
+    app.get("/notes/list/:userid", async function(req,res){
+        var notes = await Note.find({userid: req.params.userid});
+        res.json(notes);
+    });
+
+    app.post("/notes/add", async function(req,res){
+
+        const newNote = new Note({
+            id: req.body.id,
+            userid: req.body.userid,
+            title: req.body.title,
+            content: req.body.content
+        });
+        await newNote.save();
+
+        const response = { message: "New Note Created!"};
+        res.json(response);
     });
 });
 
