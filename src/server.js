dce7d24bb6299b3f4d42dbe3b145329951f0a9b1
Shubham Tiwari <st7112001@gmail.com>
2023-04-20 15:28:03 +0530

    CRUD operations

--- src/server.js
+++ src/server.js
@@ -10,17 +10,20 @@ app.use(bodyParser.urlencoded({extended: false})); // Nested objects for true
 app.use(bodyParser.json());
 
 mongoose.connect("mongodb+srv://ShubhamHarsh:Pb3zgpYV61xZv4lT@cluster0.jxblg4i.mongodb.net/?retryWrites=true&w=majority").then(function(){
+    console.log("mongo db connected");
     app.get("/", function(req,res){
-        res.send("This is the home page");
+        const response = {message: "API Working Fine"};
+        res.json(response);
     });
 
-    app.get("/notes/list/:userid", async function(req,res){
-        var notes = await Note.find({userid: req.params.userid});
+    app.post("/notes/list", async function(req,res){
+        var notes = await Note.find({userid: req.body.userid});
         res.json(notes);
     });
 
     app.post("/notes/add", async function(req,res){
 
+        await Note.deleteOne({id: req.body.id});
         const newNote = new Note({
             id: req.body.id,
             userid: req.body.userid,
@@ -29,9 +32,19 @@ mongoose.connect("mongodb+srv://ShubhamHarsh:Pb3zgpYV61xZv4lT@cluster0.jxblg4i.m
         });
         await newNote.save();
 
-        const response = { message: "New Note Created!"};
+        const response = { message: "New Note Created! id: " +req.body.userid };
         res.json(response);
     });
+
+
+    app.post("/notes/delete",async function(req,res){
+        await Note.deleteOne({id: req.body.id});
+
+        const response = { message: "Note Deleted! id: " +req.body.id };
+        res.json(response);
+    })
+
+
 });
 
 
@@ -39,5 +52,5 @@ mongoose.connect("mongodb+srv://ShubhamHarsh:Pb3zgpYV61xZv4lT@cluster0.jxblg4i.m
 // 3)-> Starting the server on a port
 
 app.listen(5000, function(){
-    console.log("Server started at PORT : 5000");
+    console.log("Server started at PORT : 5000");  
 }); // port
